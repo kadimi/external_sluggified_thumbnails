44dd736306670b0c4e3d6a629488c975b14cf169
Nabil Kadimi <nabil@kadimi.com>
2015-10-31 14:37:59 +0000

    Add option description

--- data/options.php
+++ data/options.php
@@ -5,4 +5,5 @@ paf_options( array( 'external_slugified_thumbnails_pattern' => array(
     'page' => 'external_slugified_thumbnails',
     'title' => __( 'Pattern' ),
     'default' => 'http://goo.gl/pchhEo',
+    'description' => 'You can use the tokens <code>{{POST_SLUG}}</code> and <code>{{CATEGORY_SLUG}}</code>.'
 ) ) );
